f3d958b88e0d1e5f339311d6b57bdcb60502cd23
Pascal <molin.maths@gmail.com>
2013-09-16 14:26:16 +0200

    labeling of number field ideals

--- lmfdb/WebIdeals.py
+++ lmfdb/WebIdeals.py
@@ -4,6 +4,9 @@ class NFIdeals(dict):
 
   def __init__(self, k, primelimit=100):
     self.k = k
+    self.prime = {}
+    self.tree = {}
+    #self.labels = {}
     for p in primes(primelimit):
       self._add_prime(p)
     
@@ -24,16 +27,18 @@ class NFIdeals(dict):
           label = '%i-%i'%(p,f)
           Lp[f] = [ (label, gp) ]
           self[gp] = label
+          self.prime[label] = gp
         else:
           Lf = sorted(Lp[f]) # which sort ???? by embedding ???
           Lp[f] = [ ( '%i-%i%s'%(p,f,chr(i+97)) ,gp) for i,gp in enumerate(Lf) ]
           for label,gp in Lp[f]:
             self[gp] = label
-      self[p] = Lp
+            self.prime[label] = gp
+      self.tree[p] = Lp
 
   def primelabels(self):
     R = []
-    for Lp in self.values():
+    for Lp in self.tree.values():
       for Lpf in Lp.values():
         for gp in Lpf:
           R.append(gp[0])
@@ -49,5 +54,34 @@ class NFIdeals(dict):
     if e == 1:
       return self[p]
     else:
-      return '%sp%i'%(self[p],e)
+      return '%s^%i'%(self[p],e)
 
+  def reduce(self,powers):
+    pass
+
+  # todo: need to do a right generator
+  #def ideals_of_norm(self, n):
+  #  for p,e in factor(n):
+  #    lf = self.tree[p].keys()
+  #    for a in mrange([ e//f for f in lf]):
+  #      if a*lf != e: next
+  #      for ef in Partitions(
+
+  #    for f,lf in self.tree[p]
+  #    forvec(a=vector(#wp,k,[0,ep\wp[k]]),
+  #      if(a*wp~==ep, Lp = concat(Lp, [ idealfactorback(k,fp,a)] ))
+
+  def first_ideals(self,n):
+    pass
+
+
+  def fromlabel(self, label):
+    ideal = 1 
+    for plabel in label.split('.'):
+      if '^' in plabel:
+        plabel,e = plabel.split('^')
+      else:
+        e = 1
+      p,fe = plabel.split('-')
+      ideal *= self.prime[plabel]**e
+    return ideal
